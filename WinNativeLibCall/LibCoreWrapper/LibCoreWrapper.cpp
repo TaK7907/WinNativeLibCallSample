d3a80075debd01581649a7201f57a8156530344a
TaK7907 <25642298+TaK7907@users.noreply.github.com>
2020-12-10 01:23:07 +0900

    列挙型の変換をキャストにしてみた

--- WinNativeLibCall/LibCoreWrapper/LibCoreWrapper.cpp
+++ WinNativeLibCall/LibCoreWrapper/LibCoreWrapper.cpp
@@ -32,17 +32,22 @@ bool LibCoreWrapper::CoreWrapper::RequestDataWrite(DataWriteReq req)
 	DataWriteRequest reqNative;
 	reqNative.blockNumber = req.BlockNumber;
 	reqNative.offset = req.Offset;
+#if 0
 	switch (req.DataType) {
 	case LibCoreWrapper::DataTypes::Unsigned: reqNative.type = ::DataTypes::Unsigned; break;
 	case LibCoreWrapper::DataTypes::Signed: reqNative.type = ::DataTypes::Signed; break;
 	default: throw gcnew ArgumentOutOfRangeException(req.DataType.ToString());
 	}
 	switch (req.DataSize) {
-	case LibCoreWrapper::DataLength::One:reqNative.dataSize = ::DataLength::One; break;
-	case LibCoreWrapper::DataLength::Two:reqNative.dataSize = ::DataLength::Two; break;
-	case LibCoreWrapper::DataLength::Four:reqNative.dataSize = ::DataLength::Four; break;
+	case LibCoreWrapper::DataLength::One: reqNative.dataSize = ::DataLength::One; break;
+	case LibCoreWrapper::DataLength::Two: reqNative.dataSize = ::DataLength::Two; break;
+	case LibCoreWrapper::DataLength::Four: reqNative.dataSize = ::DataLength::Four; break;
 	default: throw gcnew ArgumentOutOfRangeException(req.DataSize.ToString());
 	}
+#else
+	reqNative.type = (::DataTypes)req.DataType;
+	reqNative.dataSize = (::DataLength)req.DataSize;
+#endif
 	reqNative.value = req.Value;
 
 	return _core->RequestDataWrite(reqNative);
@@ -53,11 +58,15 @@ void LibCoreWrapper::CoreWrapper::GetDataWriteAnswer(DataWriteAns% ans)
 	::DataWriteAnswer ansNative;
 	_core->GetDataWriteAnswer(ansNative);
 
+#if 0
 	switch (ansNative.result) {
 	case ::Result::OK: ans.Result = DataWriteResult::OK; break;
 	case ::Result::NG: ans.Result = DataWriteResult::NG; break;
 	default: throw gcnew NotImplementedException();
 	}
+#else
+	ans.Result = (DataWriteResult)ansNative.result;
+#endif
 }
 
 LibCoreWrapper::CoreWrapper::CoreWrapper()
